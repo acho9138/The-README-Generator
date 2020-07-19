87394d81da43515e1b095377ec6b8fc9cafa5764
Anna C <anna.s.chong@gmail.com>
2020-07-19 11:00:15 +1000

    FIX indentation in outputed readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,45 @@
 module.exports = {
     generateTitle: function (title) {
-        if (title) {
-            return `# ${title}\n`};
+        return `# ${title}\n\n`;
     },
     generateLicenseBadge: function (license) {
-        return `![GitHub License](https://img.shields.io/badge/License-${license}-blue.svg)\n`;
+        return `![GitHub License](https://img.shields.io/badge/License-${license}-blue.svg)\n\n`;
     },
     generateDescription: function (description) {
-        return `## Description
-        ${description}\n`
+        return `## Description\n\n${description}\n\n`;
     },
     generateTableOfContents: function () {
-        return `## Table of Contents
-
-        * [Installation](#installation)
-        * [Usage](#usage)
-        * [License](#license)
-        * [Contributing](#contributing)
-        * [Tests](#tests)
-        * [Questions](#questions)\n`
+        return `## Table of Contents\n
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)\n\n`;
     },
     generateInstallation: function (installation) {
-        return `## Installation
-        
-        To install the necessary dependencies, run the following command:
-        \`\`\`
-        ${installation}
-        \`\`\`\n`
+        return `## Installation\n\nTo install the necessary dependencies, run the following command:
+\`\`\`
+${installation}
+\`\`\`\n\n`;
     },
     generateUsage: function (usage) {
-        return `## Usage
-        
-        ${usage}\n`
+        return `## Usage\n\n${usage}\n\n`;
     },
     generateLicense: function (license) {
-        return `## License
-        
-        This project is licensed under the ${license} license\n`
+        return `## License\n\nThis project is licensed under the ${license} license.\n\n`;
     },
     generateContributing: function (contributing) {
-        return `## Contributing
-        
-        ${contributing}\n`
+        return `## Contributing\n\n${contributing}\n\n`;
     },
     generateTests: function (tests) {
-        return `## Tests
-        
-        To run tests, run the following command:
-        \`\`\`
-        ${tests}
-        \`\`\`\n`
+        return `## Tests\n\nTo run tests, run the following command:
+\`\`\`
+${tests}
+\`\`\`\n\n`;
     },
     generateQuestions: function (username, email) {
-        return `## Questions
-        
-        If you have any questions about the repo, open an issue or want to discuss, contact me directly at ${email}. You can find more of my work at [${username}](https://github.com/${username}/.)`
+        return `## Questions\n\nIf you have any questions about the repo, open an issue or want to discuss, contact me directly at ${email}.
+You can find more of my work at [${username}](https://github.com/${username}/.)`;
     },
 }
